00080e658970aaf6316c3b82f9e285f4e2b25f48
maxkcy <54729511+maxkcy@users.noreply.github.com>
2020-12-01 19:35:37 -0800

    Project Start

---
+++ headless/src/main/java/com/solidplutiik/hotpotato/headless/HeadlessLauncher.java
@@ -0,0 +1,24 @@
+package com.solidplutiik.hotpotato.headless;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.solidplutiik.hotpotato.HotPotatoGameMain;
+
+/** Launches the headless application. Can be converted into a utilities project or a server application. */
+public class HeadlessLauncher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static Application createApplication() {
+		// Note: you can use a custom ApplicationListener implementation for the headless project instead of HotPotatoGameMain.
+		return new HeadlessApplication(new HotPotatoGameMain(), getDefaultConfiguration());
+	}
+
+	private static HeadlessApplicationConfiguration getDefaultConfiguration() {
+		HeadlessApplicationConfiguration configuration = new HeadlessApplicationConfiguration();
+		configuration.renderInterval = -1f; // When this value is negative, HotPotatoGameMain#render() is never called.
+		return configuration;
+	}
+}
